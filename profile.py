0fa86e0ec323ab337a64a1c53bf882ea30a034fb
Peter Yao <93311390+PeterYaoNYU@users.noreply.github.com>
2024-11-24 09:13:58 +0000

    fix duplicate interface name:

--- profile.py
+++ profile.py
@@ -16,18 +16,26 @@ for i in range(node_count):
     # node.disk_image = node_image
     nodes.append(node)
 
+# Track interface counts for each node to avoid duplicates
+interface_counts = {f"node{i}": 0 for i in range(node_count)}
+
+# Function to generate a unique interface name for a node
+def get_next_interface_name(node_name):
+    interface_counts[node_name] += 1
+    return f"eth{interface_counts[node_name]}"
+
 # Connect nodes in a ring
 for i in range(4):
     link = request.Link("link_" + str(i) + "_" + str((i + 1) % 4))
     link.bandwidth = link_bandwidth
-    link.addInterface(nodes[i].addInterface("eth" + str(i + 1)))
-    link.addInterface(nodes[(i + 1) % 4].addInterface("eth" + str((i + 1) + 1)))
+    link.addInterface(nodes[i].addInterface(get_next_interface_name(f"node{i}")))
+    link.addInterface(nodes[(i + 1) % 4].addInterface(get_next_interface_name(f"node{(i + 1) % 4}")))
 
 # Add the additional node to the ring node 0
 extra_link = request.Link("link_extra")
 extra_link.bandwidth = link_bandwidth
-extra_link.addInterface(nodes[0].addInterface("eth5"))
-extra_link.addInterface(nodes[4].addInterface("eth1"))
+extra_link.addInterface(nodes[0].addInterface(get_next_interface_name("node0")))
+extra_link.addInterface(nodes[4].addInterface(get_next_interface_name("node4")))
 
 # IP and routing setup script
 routing_script = """
