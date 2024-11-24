305e43246b9678179bb6d185da563a88f49be88b
Peter Yao <93311390+PeterYaoNYU@users.noreply.github.com>
2024-11-24 09:10:31 +0000

    improve interface naming

--- profile.py
+++ profile.py
@@ -20,14 +20,14 @@ for i in range(node_count):
 for i in range(4):
     link = request.Link("link_" + str(i) + "_" + str((i + 1) % 4))
     link.bandwidth = link_bandwidth
-    link.addNode(nodes[i])
-    link.addNode(nodes[(i + 1) % 4])
+    link.addInterface(nodes[i].addInterface("eth" + str(i + 1)))
+    link.addInterface(nodes[(i + 1) % 4].addInterface("eth" + str((i + 1) + 1)))
 
 # Add the additional node to the ring node 0
 extra_link = request.Link("link_extra")
 extra_link.bandwidth = link_bandwidth
-extra_link.addNode(nodes[0])  # Connect to node 0 in the ring
-extra_link.addNode(nodes[4])  # Additional node
+extra_link.addInterface(nodes[0].addInterface("eth5"))
+extra_link.addInterface(nodes[4].addInterface("eth1"))
 
 # IP and routing setup script
 routing_script = """
@@ -36,49 +36,49 @@ routing_script = """
 # IP and routing configuration for the nodes
 case $(hostname) in
     node0)
-        ip addr add 10.0.1.1/24 dev ens1
-        ip addr add 10.0.2.1/24 dev ens2
-        ip addr add 10.0.5.1/24 dev ens3
-        ip link set ens1 up
-        ip link set ens2 up
-        ip link set ens3 up
+        ip addr add 10.0.1.1/24 dev eth1
+        ip addr add 10.0.2.1/24 dev eth2
+        ip addr add 10.0.5.1/24 dev eth3
+        ip link set eth1 up
+        ip link set eth2 up
+        ip link set eth3 up
         ip route add 10.0.3.0/24 via 10.0.2.2
         ip route add 10.0.4.0/24 via 10.0.1.2
         ip route add 10.0.6.0/24 via 10.0.5.2
         ;;
     node1)
-        ip addr add 10.0.1.2/24 dev ens1
-        ip addr add 10.0.3.1/24 dev ens2
-        ip link set ens1 up
-        ip link set ens2 up
+        ip addr add 10.0.1.2/24 dev eth1
+        ip addr add 10.0.3.1/24 dev eth2
+        ip link set eth1 up
+        ip link set eth2 up
         ip route add 10.0.4.0/24 via 10.0.3.2
         ip route add 10.0.2.0/24 via 10.0.1.1
         ip route add 10.0.5.0/24 via 10.0.1.1
         ip route add 10.0.6.0/24 via 10.0.1.1
         ;;
     node2)
-        ip addr add 10.0.3.2/24 dev ens1
-        ip addr add 10.0.4.1/24 dev ens2
-        ip link set ens1 up
-        ip link set ens2 up
+        ip addr add 10.0.3.2/24 dev eth1
+        ip addr add 10.0.4.1/24 dev eth2
+        ip link set eth1 up
+        ip link set eth2 up
         ip route add 10.0.1.0/24 via 10.0.3.1
         ip route add 10.0.2.0/24 via 10.0.3.1
         ip route add 10.0.5.0/24 via 10.0.3.1
         ip route add 10.0.6.0/24 via 10.0.3.1
         ;;
     node3)
-        ip addr add 10.0.4.2/24 dev ens1
-        ip addr add 10.0.2.2/24 dev ens2
-        ip link set ens1 up
-        ip link set ens2 up
+        ip addr add 10.0.4.2/24 dev eth1
+        ip addr add 10.0.2.2/24 dev eth2
+        ip link set eth1 up
+        ip link set eth2 up
         ip route add 10.0.1.0/24 via 10.0.4.1
         ip route add 10.0.3.0/24 via 10.0.4.1
         ip route add 10.0.5.0/24 via 10.0.4.1
         ip route add 10.0.6.0/24 via 10.0.4.1
         ;;
     node4)
-        ip addr add 10.0.5.2/24 dev ens1
-        ip link set ens1 up
+        ip addr add 10.0.5.2/24 dev eth1
+        ip link set eth1 up
         ip route add 10.0.1.0/24 via 10.0.5.1
         ip route add 10.0.2.0/24 via 10.0.5.1
         ip route add 10.0.3.0/24 via 10.0.5.1
